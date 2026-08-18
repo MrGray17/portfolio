101671caeb10394cab376603c8cc99df2dfbf906
El Yazid Hammoubel <hammoubelyazid@gmail.com>
2026-08-18 20:43:31 +0100

    fix: give game progress UI stable ids

---
+++ game-ui-stability.js
@@ -0,0 +1,9 @@
+(() => {
+  'use strict';
+
+  const progress = document.querySelector('.experience-progress-badge');
+  if (progress) progress.id = 'experience-progress';
+
+  const achievement = document.querySelector('.experience-achievement');
+  if (achievement) achievement.id = 'experience-achievement';
+})();
